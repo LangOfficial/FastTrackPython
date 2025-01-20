000b32e6935befdcf2685b3fd664dacbc5968572
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-01-19 16:35:06 -0800

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(1, 10)
+guess = 0
+attempts = 0
+
+while number != guess:
+    guess = input(f"Guess a number between 1 and 10!\n# of attempts: {attempts}\n")
+    if guess.isdigit():
+        guess = int(guess)
+        if guess > 10 or guess < 1:
+            print("Enter a number within the range please!")
+            continue
+        if guess > number:
+            print(f"{guess} is too high!")
+        elif guess < number:
+            print(f"{guess} is too low!")
+        attempts += 1
+    else:
+        print("Enter an integer please!")
+        continue
+    
+print(f"Congratulations, you guessed {number} in {attempts} attempts!")
