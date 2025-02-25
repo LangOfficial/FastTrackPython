8099fc700eb64c8503d852cca3b15c967654890e
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-02-24 22:51:26 -0800

    Ch. 7 fixed

---
+++ exercises/07/7-6.py
@@ -0,0 +1,13 @@
+# 7-6. A Little Cart
+
+cart = set() # not {}!
+
+cart.add('apples')
+cart.add('bananas')
+cart.add('milk')
+
+cart.remove('bananas')
+
+print(cart)
+
+
---
+++ exercises/07/7-7.py
@@ -0,0 +1,6 @@
+# 7-7. Unique Words
+
+s = "apples oranges milk cheese oranges donuts milk"
+
+unique_words = set(s.split(' '))
+print(unique_words)
