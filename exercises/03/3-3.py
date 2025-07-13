e17b1dc63a0f1d8aecd4f7190cce6f7e51d24bf0
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-13 14:08:35 -0700

    Update 3-3.py

--- exercises/03/3-3.py
+++ exercises/03/3-3.py
@@ -1,10 +1,10 @@
 # 3-3. Defend Python
 
-attack = "Javascript javascript JaVAscript JAVASCRIPT" 
+attack = "JavaScript javascript JaVAscript JAVASCRIPT" 
 print(attack.lower().replace("javascript", "Python"))
 
 # or
 attack = "Javascript javascript JaVAscript JAVASCRIPT".lower()
 print(attack.replace("javascript", "Python"))
 
-# We could stack methods within the attack declaration but you get the point!
+# We could also stack methods within the attack declaration, but you get the point!
