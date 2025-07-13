41d78fbbd50dbe7f029a415faa547bbdc15dd395
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-13 10:28:35 -0700

    Update 6-1.py

--- exercises/06/6-1.py
+++ exercises/06/6-1.py
@@ -2,8 +2,8 @@
 
 best_cars = ["Honda Civic", "Toyota Camry", "Ford Mustang", "BMW", "Hellcat"]
 
-print("1st best car:", best_cars[0])
-print("2nd best car:", best_cars[1])
-print("3rd best car:", best_cars[2])
-print("4th best car:", best_cars[3])
-print("5th best car:", best_cars[4])
+print("#1", best_cars[0])
+print("#2", best_cars[1])
+print("#3", best_cars[2])
+print("#4", best_cars[3])
+print("#5", best_cars[4])
