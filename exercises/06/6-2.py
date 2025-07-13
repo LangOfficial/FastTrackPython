451dbccd38b0249107002359b2627a0974af12b7
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-13 10:32:07 -0700

    Update 6-2.py

--- exercises/06/6-2.py
+++ exercises/06/6-2.py
@@ -1,5 +1,8 @@
 # 6-2. Top 3 Movies
 
 best_cars = ["Honda Civic", "Toyota Camry", "Ford Mustang", "BMW", "Hellcat"]
+bestest_cars = best_cars[:3]
+print("The top 3 best cars are", bestest_cars)
 
+# could also do
 print("The top 3 best cars are", best_cars[:3])
