ea5bb879a2019269cbc8a1251448c6b0e39fffe4
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-13 13:49:50 -0700

    Update 9-1.py

--- exercises/09/9-1.py
+++ exercises/09/9-1.py
@@ -2,5 +2,5 @@
 
 favorite_food = 'burritos'
 
-if favorite_food == 'burritos':
+if favorite_food == 'burritos': # If the comparison string was changed (e.g., tacos), this won't run.
   print("Your favorite food is burritos.")
