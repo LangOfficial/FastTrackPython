3e110ca8abfa22cc365757e0ca6125b9e9187d92
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-14 14:40:00 -0700

    Update 10-3.py

--- exercises/10/10-3.py
+++ exercises/10/10-3.py
@@ -1,7 +1,7 @@
 # 10-3: Cart Total
 
 sub_total = 0
-tax = .0725 # if you want to apply tax
+tax = .0725 # If you want to apply sale tax (California's sales tax as of 2025)
 
 while True:
   price = input("Enter a price (or 'done' to finish): ")
