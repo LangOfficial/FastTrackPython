300e32aa732f5bdb424b8fe01b0beca5a0241198
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-14 14:40:22 -0700

    Update 10-4.py

--- exercises/10/10-4.py
+++ exercises/10/10-4.py
@@ -6,9 +6,9 @@ grades_a = 0
 for grade in grades:
     grades_a += 1
     if grade != 'A':
-        # they won't know you didn't get an A :)
+        # They won't know you didn't get an A :)
         continue
     print("You got an A!")
 
-if grades_a == len(grades): # checker that you did it right.
+if grades_a == len(grades): # Checker that you did it right.
     print("Here's $1,000 for your hard work!")
