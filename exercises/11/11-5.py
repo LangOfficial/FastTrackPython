e6fa5bdae550bd8b6d434fe6e2cfc22045c9972e
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-14 15:29:41 -0700

    Update 11-5.py

--- exercises/11/11-5.py
+++ exercises/11/11-5.py
@@ -3,8 +3,8 @@
 menu = {"burger": 7.99, "pizza": 9.99, "cookie": 1.25}
 total_price = 0
 
-# since my menu is only 3 items, we could take the sum of the prices
-# but let's pretend it's not the whole menu haha
+# Since my menu is only 3 items, we could take the sum of the prices
+# But let's pretend it's not the whole menu haha
 # print(sum(menu.values()))
 
 total_price += menu['burger']
@@ -13,11 +13,11 @@ total_price += menu['cookie']
 
 print(f'You ordered a burger, pizza, and cookie. The total comes out to be ${total_price}.')
 
-# after the exercises, we learned about iterating over dictionaries
-# just in case you wanted to see this!
+# After the exercises, we learned about iterating over dictionaries
+# Just in case you wanted to see this!
 
-food_items = list(menu.keys()) # menu.keys() gets a dict_keys object. Since indexing doesn't work on it, we want to treat it like a list. 
-prices = list(menu.values())[:3] # first 3 prices
+food_items = list(menu.keys())[:3] # menu.keys() gets a dict_keys object. Since indexing doesn't work on that type of data, we want to treat it like a list. 
+prices = list(menu.values())[:3] 
 total_price_2 = sum(prices)
 # total_price_2 = sum(menu[item] for item in ordered_items) # if you know comprehension
 print(f"You ordered a {food_items[0]}, {food_items[1]}, and {food_items[2]}. The total comes out to be ${total_price_2}.")
