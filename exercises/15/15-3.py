2c4ddb23cd176071f4613dec3b2d43e052f20c94
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-15 14:14:52 -0700

    Update 15-3.py

--- exercises/15/15-3.py
+++ exercises/15/15-3.py
@@ -6,15 +6,23 @@ matrix = [
     [7, 8, 9]
 ]
 
-# list comprehension method (recommended method)
+# list comprehension method 
 total = sum([sum(row) for row in matrix])
-# sum(sum(row) for row in matrix) Instead of list comp, we use a generator object. A generator is a special type of iterable that is more memory efficent than lists. This is not covered in the course.
+# OR
+# sum(sum(row) for row in matrix) (best method in terms of speed and mmeory)
+# Instead of list comp, we use a generator object. 
+# A generator is a special type of iterable that is more memory efficent than lists. This is not covered in the course.
 
 # increment method
 total = 0
 total += sum(matrix[0])
 total += sum(matrix[1])
 total += sum(matrix[2])
+
+# increment method #2 (2nd best method in terms of speed and memory efficiency. It is more flexible than generator comprehension since it can include more complex logic with conditionals and more.)
+total = 0
+for row in matrix:
+    total += sum(row)
 
 # addition method
 total = sum(matrix[0]) + sum(matrix[1]) + sum(matrix[2])
