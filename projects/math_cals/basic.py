ed1752dc3b31b16dad8bf5dbaf130d3ed5f14ce4
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-07-19 22:39:38 -0700

    Update basic.py

--- projects/math_cals/basic.py
+++ projects/math_cals/basic.py
@@ -4,9 +4,11 @@ first_number = input("Enter your first number: ")
 second_number = input("Enter your second number: ")
 operator = input("Enter an operator (+, -, *, /, **, or %): ")
 
-result = eval(f"{first_number} {operator} {second_number}")
+# Don't use eval() in production code due to being able to execute arbitrary code if misused!
+# Even if it's non-production code, I highly recommend not using it to avoid it becoming a habit.
+result = eval(f"{first_number} {operator} {second_number}") # For learning purposes only
 
 print("Calculating...")
-time.sleep(.5) # takes num in seconds
+time.sleep(.5) # Takes number in seconds
 
 print(f"{first_number} {operator} {second_number} = {result}")
