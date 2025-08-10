597f242ec8e75a7e3bc5a6e1181facd5b0cacbdb
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-10 13:41:27 -0700

    Update 9-6.py

--- exercises/09/9-6.py
+++ exercises/09/9-6.py
@@ -3,11 +3,12 @@
 is_member = True
 spending = 120
 
-if is_member and spending > 100:
+if is_member or spending > 100:
   print("You get a discount!")
 else:
   print("Have a good day!")
 
 # ternary
-discount = "You get a discount!" if is_member and spending > 100 else "Have a good day!"
+discount = "You get a discount!" if is_member or spending > 100 else "Have a good day!"
 print(discount)
+
