37e8f9436dd2b8facac1353eecb11942f2959a4f
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-10 13:43:06 -0700

    Update 9-7.py

--- exercises/09/9-7.py
+++ exercises/09/9-7.py
@@ -7,3 +7,7 @@ if grade_letter == 'A' and avg_test_score >= 85:
   print("You can skip the exam!")
 else:
   print("Good luck!")
+
+# ternary
+skip_final = "You can skip the exam!" if grade_letter == 'A' and avg_test_score >= 85 else "Good luck!"
+print(skip_final)
