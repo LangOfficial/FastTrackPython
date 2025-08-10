b7aa09482942b1fec539a87f932f567f2ff28f8a
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-10 13:40:38 -0700

    Update 9-9.py

--- exercises/09/9-9.py
+++ exercises/09/9-9.py
@@ -7,5 +7,10 @@ for alien_color in alien_colors:
     print("Hello!")
   elif alien_color == 'yellow':
     print("We should keep an eye on this one.")
-  else:
+  elif alien_color == 'red':
     print("Shoot it down!")
+  # else:
+  #   print("Shoot it down!")
+  # An else statement is fine for this scenario, but if we want to focus on the three types of aliens, it's better to specify them.
+  # else can also be used to specifically anything but a green, yellow, or red alien.
+  
