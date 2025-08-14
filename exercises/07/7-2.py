800078ff3fe20351cee2d24fce36a9ce4240d76a
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-14 09:23:54 -0700

    Update 7-2.py

--- exercises/07/7-2.py
+++ exercises/07/7-2.py
@@ -4,8 +4,12 @@ friends = ['Greg', 'Zachary', 'Andrew', 'Alyssa', 'Jimmy', 'Beatrice']
 
 print(sorted(friends, reverse=True)) # best
 
-# some other ways
+# Some other ways
+
 # print(sorted(friends)[::-1])
+
 # friends.sort(reverse=True)
 # print(friends)
+
 # print(list(reversed(sorted(friends))))
+
