c6a8ca6b4ce06483cb54c6236bbd765753ee3e3e
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-17 12:58:30 -0700

    Update 17-2.py

--- exercises/17/17-2.py
+++ exercises/17/17-2.py
@@ -9,25 +9,41 @@ try:
   second_number = float(input("Enter your second number: "))
 except ValueError:
   print("Please enter a number.")
-  exit() # ends program
+  exit() # Ends program because without it, it will move on to ask for the operator. For this one, there's no chance for reinputting.
 except:
   print("Something unexpected happened. Please try again.")
   exit()
 
+def calculator(num_1, num_2, operator):
+    if operator == '+':
+        return num_1 + num_2
+    elif operator == '-':
+        return num_1 - num_2
+    elif operator == '*':
+        return num_1 * num_2
+    elif operator == '/':
+        return num_1 / num_2
+    elif operator == '**':
+        return num_1 ** num_2
+    elif operator == '%':
+        return num_1 % num_2
+    else:
+        return 'Invalid Operator!'
+
 operator = input("Enter an operator (+, -, *, /, **, or %): ")
-
-result = eval(f"{first_number} {operator} {second_number}")
-
+result = calculator(first_number, second_number, operator)
+  
 print("Calculating...")
-time.sleep(.5) # takes num in seconds
-
+time.sleep(.5) # Takes num in seconds
+  
 print(f"{first_number} {operator} {second_number} = {result}")
+
 
 # hypotenuse calc
 def get_number_input(prompt): 
     """Continuously ask the user until they enter a number"""
-    # had to individually check for each input because if we had multiple inputs being checked and one gets excepted, the user has to start over
-    # ex: they typed a word in the 2nd input and the while loop restarts since it didn't end. That makes the user start back at the 1st input
+    # Had to individually check for each input because if we had multiple inputs being checked and one gets excepted, the user has to start over
+    # Eg: they typed a word in the 2nd input and the while loop restarts since it didn't end. That makes the user start back at the 1st input
     while True:
         try:
             return float(input(prompt))
@@ -43,4 +59,5 @@ hypotenuse = math.sqrt(a ** 2 + b ** 2)
 print("Calculating...") 
 time.sleep(.5)
  
+
 print(f"The hypotenuse that forms with legs {a} and {b} is {round(hypotenuse, 1)}")
