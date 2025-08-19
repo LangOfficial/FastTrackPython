ed4db2a4ae9480fce4c08f6b7c5f9af1b2538761
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-19 07:50:15 -0700

    Update 4-1.py

--- exercises/04/4-1.py
+++ exercises/04/4-1.py
@@ -1,6 +1,7 @@
 # 4-1. Average Grade
 
 average = (86 + 72 + 95 + 91 + 64) / 5 # Remember PEMDAS
-# the parentheses are needed to calculate the addition first before dividing
+# The parentheses are needed to calculate the addition first before dividing
+
 
 print(f"Johnny's average grade is {average}.")
