aaabb08011a07842910770a1e563999933a02c87
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2025-08-19 07:52:39 -0700

    Update 6-2.py

--- exercises/06/6-2.py
+++ exercises/06/6-2.py
@@ -4,5 +4,6 @@ best_cars = ["Honda Civic", "Toyota Camry", "Ford Mustang", "BMW", "Hellcat"]
 bestest_cars = best_cars[:3]
 print("The top 3 best cars are", bestest_cars)
 
-# could also do
+# Could also do
 print("The top 3 best cars are", best_cars[:3])
+
